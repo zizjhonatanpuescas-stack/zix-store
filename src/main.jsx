a449fed8a8bc299b59e194d8e0eee93e94a829b4
zizjhonatanpuescas-stack <zizjhonatanpuescas@gmail.com>
2026-09-08 18:15:57 -0500

    fix: scroll al inicio al navegar entre paginas

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import ScrollToTop from './components/common/ScrollToTop.jsx';
 import { StoreProvider } from './context/StoreContext.jsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <StoreProvider>
         <App />
       </StoreProvider>
